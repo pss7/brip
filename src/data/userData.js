2ea8515111bc4f368771b5866d5251b329d5e14f
parksangseok <>
2025-01-19 16:00:34 +0900

    fix: 프로팔 이미지 경로 수정

--- src/data/userData.js
+++ src/data/userData.js
@@ -2,5 +2,5 @@ export const userData = {
   name: '홍길동',
   email: 'test@email.com',
   password: '123',
-  profileImg: '/src/assets/images/common/Profile_Img.svg',
+  profileImg: '/assets/images/common/Profile_Img.svg',
 };
