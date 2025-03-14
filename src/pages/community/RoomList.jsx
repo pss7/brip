f64b1e9adbe2d91c1beaeea8f63caa306459566b
parksangseok <park0157@naver.com>
2025-03-14 16:21:16 +0900

    commit

--- src/pages/community/RoomList.jsx
+++ src/pages/community/RoomList.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import CreateRoomModal from './CreateRoomModal.JSX';
+
 import Loading from '../../components/Loading';
+import CreateRoomModal from './CreateRoomModal';
 
 const RoomList = () => {
   const [roomList, setRoomList] = useState([]);
